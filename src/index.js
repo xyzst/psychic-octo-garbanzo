cb0296d0c6e915aaa88df01218eb47c4ce05fcb4
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-22 21:26:07 -0500

    Leverage axios global interceptors to define common request and response handling. Define default base url and headers as well (authorization, accept)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+axios.defaults.headers.common["Authorization"] =
+  "some auth token, probably shouldnt use this in prod";
+axios.defaults.headers.common["Accept"] = "application/json";
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    // Add headers here for EVERY request
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  request => {
+    console.log(request);
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
