8fe7b0664b6a25b7573e67f5d39d54962b6b2dbe
Darren Rambaud <xyzst@users.noreply.github.com>
2019-06-30 22:14:55 -0500

    Setup react router, refactor existing components into containers folder

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 // import axios from "axios";
 import axios from "../../axios";
 import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
+import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -64,12 +64,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
