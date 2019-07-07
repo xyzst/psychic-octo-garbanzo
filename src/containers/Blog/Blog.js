de772cd8a4e2dea1cbce0a3b2bbbb52bc110338d
Darren Rambaud <xyzst@users.noreply.github.com>
2019-07-07 15:57:12 -0500

    Leverage Redirect component from react-router-dom to specify the root path to point to /posts by default

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import NewPost from "./NewPost/NewPost";
-import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
   render() {
@@ -47,6 +46,8 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+          {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
